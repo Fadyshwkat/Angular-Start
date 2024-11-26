84d76aca85334fb6e547ad39caaab824383442b2
Fadyshwkat <fadyshwkat@gmail.com>
2024-11-26 21:26:46 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { PortofolioComponent } from '../portofolio/portofolio.component';
+import { ContactComponent } from '../contact/contact.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'' , redirectTo:'home' , pathMatch:'full'},
+    {path:'home' , component:HomeComponent , title:'Home'},
+    {path:'about' , component:AboutComponent , title:'About'},
+    {path:'portfolio' , component:PortofolioComponent , title:'Portfolio'},
+    {path:'contact' , component:ContactComponent , title:'Contact'},
+    {path:'**' , component:NotFoundComponent , title:'Not Found'},
+
+];
